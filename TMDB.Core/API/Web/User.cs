00d5e2dc6a1b4e1acfe5cc3d1d0bf2984ebb430a
Shark <sharkcffg@gmail.com>
2021-03-24 09:58:48 +0300

    api

---
+++ TMDB.Core/API/Web/User.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TMDB.Core.API.Web
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        [Required]
+        public int id { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [StringLength(50)]
+        public string Username { get; set; }
+    }
+}
